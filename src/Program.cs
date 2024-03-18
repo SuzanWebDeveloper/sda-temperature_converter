dcfbd0d13e54bb65272422d2a59704f5760db2dd
SuzanWebDeveloper <iamstrongwebdev@gmail.com>
2024-03-18 14:00:29 +0300

    Implemented TempConvert function - basic conversion

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Temperature
+{
+  public static string TempConvert(int amount, string unit)
+  {
+    string result;
+    if (unit == "F")
+    {
+      int converted = (amount - 32) * 5 / 9;
+      result = converted + " C";
+    }
+    else
+    {
+      int converted = (amount * 9 / 5) + 32;
+      result = converted + " F";
+    }
+    return result;
+  }
+
+  public static void Main(String[] args)
+  {
+    string result1 = TempConvert(32, "F");
+    Console.WriteLine($"{result1}");
+    string result2 = TempConvert(100, "C");
+    Console.WriteLine($"{result2}");
+  }
+
+}
