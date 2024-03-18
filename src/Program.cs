a16731151a3db33a6acef98150601f72ffb4cb42
SuzanWebDeveloper <iamstrongwebdev@gmail.com>
2024-03-18 16:47:45 +0300

    prompt user for temperature value and scale C/F

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-
-// Modify the application to prompt the user for a temperature value and a scale (Celsius 'C' or Fahrenheit 'F').
-// // Expected output
-// Enter a temperature and its unit (C or F):
-// > 45 F
-// Converted: 45 F = 7.22 C
-
 class Temperature
 {
-  public static string TempConvert(int amount, string unit)
+  public static string TempConvert(double amount, string unit)
   {
     string result;
     if (unit == "F")
     {
-      int converted = (amount - 32) * 5 / 9;
-      result = converted + " C";
+      double converted = (amount - 32) * 5 / 9;
+      result = Math.Round(converted, 2) + " C";
     }
     else
     {
-      int converted = (amount * 9 / 5) + 32;
-      result = converted + " F";
+      double converted = (amount * 9 / 5) + 32;
+      result = Math.Round(converted, 2) + " F";
     }
     return result;
   }
@@ -27,10 +20,10 @@ class Temperature
   public static void Main(String[] args)
   {
     string result;
-    result = TempConvert(32, "F");
-    Console.WriteLine($"{result}");
-    result = TempConvert(100, "C");
-    Console.WriteLine($"{result}");
+    Console.WriteLine("Enter a temperature and its unit(C or F):");
+    string input = Console.ReadLine() ?? "";
+    string[] inputSplitted = input.Split(" ");
+    result = TempConvert(Convert.ToDouble(inputSplitted[0]), inputSplitted[1]);
+    Console.WriteLine($"Converted: {input} = {result}");
   }
-
 }
