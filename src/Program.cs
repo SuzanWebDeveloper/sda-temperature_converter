d312e2caf912729da0cf07aab4fa966f1ca76854
SuzanWebDeveloper <iamstrongwebdev@gmail.com>
2024-03-19 03:41:23 +0300

    added try catch Exception

--- src/Program.cs
+++ src/Program.cs
@@ -19,34 +19,42 @@ class Temperature
 
   public static void Main(String[] args)
   {
-    while (true)
+    try
     {
-      Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
-      string input = Console.ReadLine() ?? "";
-      string[] inputSplitted = input.Split(" ");
-      if (input.ToLower() == "quit")
+      while (true)
       {
-        Console.WriteLine("Program terminated.");
-        break;
+        Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
+        string input = Console.ReadLine() ?? "";
+        string[] inputSplitted = input.Split(" ");
+        if (input.ToLower() == "quit")
+        {
+          Console.WriteLine("Program terminated.");
+          break;
+        }
+        if (inputSplitted.Length != 2)
+        {
+          Console.WriteLine("Invalid input. Please enter a temperature and its unit (C or F) \n");
+          continue;
+        }
+        if (!double.TryParse(inputSplitted[0], out double tempValue))
+        {
+          Console.WriteLine("Invalid input. Please enter a numeric temperature. \n");
+          continue;
+        }
+        string tempUnit = inputSplitted[1].ToUpper();
+        if (tempUnit != "C" && tempUnit != "F")
+        {
+          Console.WriteLine("Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit. \n");
+          continue;
+        }
+        string result = TempConvert(tempValue, tempUnit);
+        Console.WriteLine($"Converted: {input} = {result} \n");
       }
-      if (inputSplitted.Length != 2)
-      {
-        Console.WriteLine("Invalid input. Please enter a temperature and its unit (C or F) \n");
-        continue;
-      }
-      if (!double.TryParse(inputSplitted[0], out double tempValue))
-      {
-        Console.WriteLine("Invalid input. Please enter a numeric temperature. \n");
-        continue;
-      }
-      string tempUnit = inputSplitted[1].ToUpper();
-      if (tempUnit != "C" && tempUnit != "F")
-      {
-        Console.WriteLine("Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit. \n");
-        continue;
-      }
-      string result = TempConvert(tempValue, tempUnit);
-      Console.WriteLine($"Converted: {input} = {result} \n");
     }
+    catch (Exception e)
+    {
+      Console.WriteLine(e.Message);
+    }
+
   }
 }
